e53456f6a3fbd6b4f67bd573be717330c0afaf83
AbderrahmanAboumouslim <abdourokinos@gmail.com>
2022-02-09 14:09:16 +0100

    add interface to prevent typos

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
 import axios from "axios";
 const url = "https://jsonplaceholder.typicode.com/users/1";
-axios.get(url).then((res) => console.log(res.data));
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+axios.get(url).then((res) => {
+  const user: User = res.data; //other way:   const user: User = res.data as User;
+
+  const { id, name, email } = user;
+  console.log(`
+   User's name: ${name}
+   ID is : ${id}
+   and email: ${email} `);
+});
